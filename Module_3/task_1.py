9629381d647b2f6a48e36479274f90ac87cbe912
KP8990 <kunj8990@gmail.com>
2025-06-23 15:10:30 +0530

    Add files via upload

--- Module_3/task_1.py
+++ Module_3/task_1.py
@@ -1,1 +1,10 @@
+#Task 1: Calculate Factorial Using a Function 
 
+def fact(n):
+    if(n==1):
+        return 1
+    else:
+        return n*fact(n-1)
+
+i=int(input("Enter a number: "))
+print(f"Factorial of {i} is : {fact(i)}")
---
+++ Module_3/task_2.py
@@ -0,0 +1,8 @@
+#Task 2: Using the Math Module for Calculations
+
+import math
+
+i=int(input("Enter a number : "))
+print(f"Square root: {math.sqrt(i)}")
+print(f"Logarithm : {math.log(i)}")
+print(f"Sine : {math.sin(i)}")
