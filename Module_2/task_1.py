805214a0f597c62a158fe8de9a484e73c2df2bd8
KP8990 <kunj8990@gmail.com>
2025-06-23 15:06:49 +0530

    Add files via upload

--- Module_2/task_1.py
+++ Module_2/task_1.py
@@ -1,1 +1,7 @@
+#Task 1: Check if a Number is Even or Odd
+i=int(input("Enter a number: "))
+if (i%2==0):
+    print(i,"is even number.")
 
+else:
+    print(i,"is odd number.")
---
+++ Module_2/task_2.py
@@ -0,0 +1,6 @@
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+x=0 
+for i in range(0,50):
+    x+=(i+1)
+
+print("The sum of first 50 numbers is :",x);
